9d2f88a2d00bdd6d1c0b47f27a7f79332f066d46
dido18 <davideneri18@gmail.com>
2018-09-10 11:51:01 +0200

    basepath snappy with single and double quotes

--- sin/config/snappy.php
+++ sin/config/snappy.php
@@ -8,7 +8,8 @@ return array(
         // 'binary'  => '/usr/local/bin/wkhtmltopdf',
         // 'binary' => base_path("vendor\h4cc\wkhtmltopdf-amd64\bin\wkhtmltopdf-amd64"),
         // https://github.com/barryvdh/laravel-snappy/issues/60
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        // https://answers.microsoft.com/en-us/windows/forum/windows_10-performance-winpc/the-program-cant-start-because-msvcp120dll-is/5f4016ca-2c7c-49de-bf64-40a06ab004d6
+        'binary' => base_path('"vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf"'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
