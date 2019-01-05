c9da718d8970c5f083dfe27320d960ae7b7ecaa9
dido18 <davideneri18@gmail.com>
2019-01-05 14:58:15 +0100

    patente: ifset datipersonali

--- sin/resources/views/patente/search.blade.php
+++ sin/resources/views/patente/search.blade.php
@@ -108,12 +108,17 @@
           @foreach($patenti as $patente)
           <tr hoverable>
           <td> 
+          @if($patente->persona->datipersonali)
               @isset($patente->persona->datipersonali->nome)
-                 {{ $patente->persona->datipersonali->nome}}
-              @endisset
-              @isset($patente->persona->datipersonali->cognome)
-                {{$patente->persona->datipersonali->cognome}}
-              @endisset
+                    {{ $patente->persona->datipersonali->nome}}
+                  @endisset
+                  @isset($patente->persona->datipersonali->cognome)
+                    {{$patente->persona->datipersonali->cognome}}
+                  @endisset
+          @else
+             {{ $patente->persona->nominativo}}
+          @endif
+              
                 @if($patente->stato == 'commissione')
                   <span class="badge badge-warning">C.</span>
                 @endif
