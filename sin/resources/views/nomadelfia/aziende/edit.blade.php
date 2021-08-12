ac240fc7b3e8bbb8f46b21ccf93f0d42a5bea4e8
dneri <d.neri@zerynth.com>
2021-08-12 15:50:24 +0200

    fix add lavoratore wuth vue

--- sin/resources/views/nomadelfia/aziende/edit.blade.php
+++ sin/resources/views/nomadelfia/aziende/edit.blade.php
@@ -4,5 +4,5 @@
 	<sin-header title="{{'Modifica azienda: '.$azienda->nome_azienda }}">
 		
 	</sin-header>
-	<azienda-edit base_url='/api/nomadelfia/aziende'  url_azienda_edit="{{ route('api.nomadeflia.azienda.edit', $azienda->id ) }}" url_persona url_mansioni="{{ route('api.nomadeflia.azienda.mansioni') }}" url_stati="{{ route('api.nomadeflia.azienda.stati') }}" url_modifica_lavoratore="{{ route('api.nomadeflia.azienda.modifica.lavoratore') }}" id_azienda="{{$azienda->id}}"></azienda-edit>
+	<azienda-edit url_aggiungi="{{route('api.nomadelfia.azienda.aggiungi.lavoratore')}}" base_url='/api/nomadelfia/aziende'  url_azienda_edit="{{ route('api.nomadeflia.azienda.edit', $azienda->id ) }}" url_persona url_mansioni="{{ route('api.nomadeflia.azienda.mansioni') }}" url_stati="{{ route('api.nomadeflia.azienda.stati') }}" url_modifica_lavoratore="{{ route('api.nomadeflia.azienda.modifica.lavoratore') }}" id_azienda="{{$azienda->id}}"></azienda-edit>
 @endsection
