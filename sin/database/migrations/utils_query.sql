58c518348f8e2a414dbdbfe2ac50887db0758a00
dneri <d.neri@zerynth.com>
2021-11-22 23:46:10 +0100

    delete persone_cateogrie utils query

--- sin/database/migrations/utils_query.sql
+++ sin/database/migrations/utils_query.sql
@@ -1,10 +1,4 @@
 
--- Aggioran tutte le data di entrata delle perosne conla data di nascita
-UPDATE persone_categorie, persone
-SET persone_categorie.data_inizio = persone.data_nascita
-Where persone_categorie.persona_id = persone.id AND persone_categorie.categoria_id = 1 AND persone_categorie.stato = '1';
-
-
 -- Aggiunge per tutti i figli minorenni senso uno stato familiare
 -- un nuovo stato (nubile o celibe) con la data di nascita
 INSERT INTO persone_stati (persona_id, stato_id, stato, data_inizio)
