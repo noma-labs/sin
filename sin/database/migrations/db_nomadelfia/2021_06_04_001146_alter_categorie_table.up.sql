40b884dab58ec83529f24b1b98a648837fbd70c9
dneri <d.neri@zerynth.com>
2021-11-22 23:25:47 +0100

    removed categoria seeders and sql import

--- sin/database/migrations/db_nomadelfia/2021_06_04_001146_alter_categorie_table.up.sql
+++
@@ -1,9 +0,0 @@
--- Run the migrations
-ALTER TABLE db_nomadelfia.persone_categorie DROP FOREIGN KEY persone_categorie_ibfk_1;
-ALTER TABLE db_nomadelfia.persone_categorie DROP FOREIGN KEY persone_categorie_ibfk_2;
-
-ALTER table persone_categorie ADD uuid MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-
-ALTER TABLE `persone_categorie`
-  ADD CONSTRAINT `persone_categorie_ibfk_1` FOREIGN KEY (`persona_id`) REFERENCES `persone` (`id`),
-  ADD CONSTRAINT `persone_categorie_ibfk_2` FOREIGN KEY (`categoria_id`) REFERENCES `categorie` (`id`);
