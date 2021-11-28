adaa282b1a10b4c50772ad2041675041b58721d4
dneri <d.neri@zerynth.com>
2021-11-28 22:06:00 +0100

    fix migrations biblioteca

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
+
 #!/bin/bash
 docker-compose up -d app
-
-
 #docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Actions --testdox
-#docker-compose exec app /usr/bin/php /var/www/html/vendor/bin/phpunit  --configuration /var/www/html/phpunit.xml --testsuite Unit --testdox
+docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Unit --testdox
 #docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Feature --testdox
 #docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Http --testdox
