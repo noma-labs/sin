4c2c4fc34304763c6a74877ce091998f19057692
dneri <d.neri@zerynth.com>
2021-12-04 11:04:11 +0100

    try fix cariche

--- sin/resources/views/nomadelfia/cariche/index.blade.php
+++ sin/resources/views/nomadelfia/cariche/index.blade.php
@@ -29,101 +29,101 @@
         </div>
     </div>
 
-    <div class="col-md-3 card-deck">
-        <div class="card" >
-            <div class="card-header">
-                Solidarietà Nomadelfia ODV
-            </div>
-            <div class="card-body">
-                <ul>
-                    @foreach($sol as $key => $membri)
-                        <li>{{$key}} </li>
-                        <ul>
-                            @foreach($membri as $m)
-                                <li>
-                                @if (empty($m->nominativo))
-                                    I have NO record!
-                                @else
-                                    @include("nomadelfia.templates.persona", ['persona' => $m])
-                                @endif
-                                </li>
-                            @endforeach
-                        </ul>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="card-footer">
+{{--    <div class="col-md-3 card-deck">--}}
+{{--        <div class="card" >--}}
+{{--            <div class="card-header">--}}
+{{--                Solidarietà Nomadelfia ODV--}}
+{{--            </div>--}}
+{{--            <div class="card-body">--}}
+{{--                <ul>--}}
+{{--                    @foreach($sol as $key => $membri)--}}
+{{--                        <li>{{$key}} </li>--}}
+{{--                        <ul>--}}
+{{--                            @foreach($membri as $m)--}}
+{{--                                <li>--}}
+{{--                                @if (empty($m->nominativo))--}}
+{{--                                    I have NO record!--}}
+{{--                                @else--}}
+{{--                                    @include("nomadelfia.templates.persona", ['persona' => $m])--}}
+{{--                                @endif--}}
+{{--                                </li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    @endforeach--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--            <div class="card-footer">--}}
 {{--                <a href="{{ route('nomadelfia.famiglie') }}" class="btn btn-primary">Modifica</a>--}}
-            </div>
-        </div>
-    </div>
-    <div class="col-md-2 card-deck">
-        <div class="card" >
-            <div class="card-header">
-                Fondazione Nomadelfia
-            </div>
-            <div class="card-body">
-                <ul>
-                    @foreach($fon as $key => $membri)
-                        <li>{{$key}} </li>
-                        <ul>
-                            @foreach($membri as $m)
-                                <li>@include("nomadelfia.templates.persona", ['persona' => $m])  </li>
-                            @endforeach
-                        </ul>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="card-footer">
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--    <div class="col-md-2 card-deck">--}}
+{{--        <div class="card" >--}}
+{{--            <div class="card-header">--}}
+{{--                Fondazione Nomadelfia--}}
+{{--            </div>--}}
+{{--            <div class="card-body">--}}
+{{--                <ul>--}}
+{{--                    @foreach($fon as $key => $membri)--}}
+{{--                        <li>{{$key}} </li>--}}
+{{--                        <ul>--}}
+{{--                            @foreach($membri as $m)--}}
+{{--                                <li>@include("nomadelfia.templates.persona", ['persona' => $m])  </li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    @endforeach--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--            <div class="card-footer">--}}
 {{--                <a href="{{ route('nomadelfia.famiglie') }}" class="btn btn-primary">Modifica</a>--}}
-            </div>
-        </div>
-    </div>
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
-    <div class="col-md-2 card-deck">
-        <div class="card" >
-            <div class="card-header">
-                Cooperativa Agricola Culturale
-            </div>
-            <div class="card-body">
-                <ul>
-                    @foreach($agr as $key => $membri)
-                        <li>{{$key}} </li>
-                        <ul>
-                            @foreach($membri as $m)
-                                <li>@include("nomadelfia.templates.persona", ['persona' => $m])  </li>
-                            @endforeach
-                        </ul>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="card-footer">
+{{--    <div class="col-md-2 card-deck">--}}
+{{--        <div class="card" >--}}
+{{--            <div class="card-header">--}}
+{{--                Cooperativa Agricola Culturale--}}
+{{--            </div>--}}
+{{--            <div class="card-body">--}}
+{{--                <ul>--}}
+{{--                    @foreach($agr as $key => $membri)--}}
+{{--                        <li>{{$key}} </li>--}}
+{{--                        <ul>--}}
+{{--                            @foreach($membri as $m)--}}
+{{--                                <li>@include("nomadelfia.templates.persona", ['persona' => $m])  </li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    @endforeach--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--            <div class="card-footer">--}}
 {{--                <a href="{{ route('nomadelfia.famiglie') }}" class="btn btn-primary">Modifica</a>--}}
-            </div>
-        </div>
-    </div>
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
-    <div class="col-md-2 card-deck">
-        <div class="card" >
-            <div class="card-header">
-                Cooperativa Culturale
-            </div>
-            <div class="card-body">
-                <ul>
-                    @foreach($cul as $key => $membri)
-                        <li>{{$key}} </li>
-                        <ul>
-                            @foreach($membri as $m)
-                                <li>@include("nomadelfia.templates.persona", ['persona' => $m])  </li>
-                            @endforeach
-                        </ul>
-                    @endforeach
-                </ul>
-            </div>
-            <div class="card-footer">
+{{--    <div class="col-md-2 card-deck">--}}
+{{--        <div class="card" >--}}
+{{--            <div class="card-header">--}}
+{{--                Cooperativa Culturale--}}
+{{--            </div>--}}
+{{--            <div class="card-body">--}}
+{{--                <ul>--}}
+{{--                    @foreach($cul as $key => $membri)--}}
+{{--                        <li>{{$key}} </li>--}}
+{{--                        <ul>--}}
+{{--                            @foreach($membri as $m)--}}
+{{--                                <li>@include("nomadelfia.templates.persona", ['persona' => $m])  </li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    @endforeach--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--            <div class="card-footer">--}}
 {{--                <a href="{{ route('nomadelfia.famiglie') }}" class="btn btn-primary">Modifica</a>--}}
-            </div>
-        </div>
-    </div>
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 </div>
 @endsection
