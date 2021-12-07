7c2598db6be5e069880d8ee532d6dc49e326a062
dneri <d.neri@zerynth.com>
2021-12-07 08:03:44 +0100

    fix route famiglia dettaglio

--- sin/resources/views/nomadelfia/gruppifamiliari/edit.blade.php
+++ sin/resources/views/nomadelfia/gruppifamiliari/edit.blade.php
@@ -82,7 +82,7 @@
                             @else
                             <div class="col-md-6">
                                 <div class="font-weight-bold mt-2">
-                                        <a href="{{route('nomadelfia.famiglia.dettaglio',['idFamiglia'=>$famiglia_single->famiglia_id])}}"> 
+                                        <a href="{{route('nomadelfia.persone.dettaglio',['id'=>$famiglia_single->famiglia_id])}}">
                                                 {{$famiglia_single->nominativo}}
                                         </a>
                                 </div> 
@@ -104,7 +104,7 @@
                                 <div class="col-md-6">
                                     <div class="font-weight-bold mt-2">
                                          Fam. 
-                                         <a href="{{route('nomadelfia.famiglia.dettaglio',['idFamiglia'=>$famiglia_id])}}"> 
+                                         <a href="{{route('nomadelfia.persone.dettaglio',['id'=>$famiglia_id])}}">
                                                 {{App\Nomadelfia\Models\Famiglia::find($famiglia_id)->nome_famiglia}} 
                                          </a>({{count($componenti)}})
                                        
