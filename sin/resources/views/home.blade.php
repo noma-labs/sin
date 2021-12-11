f0a83009ec99fb8f06f38fd6a955b2622ffa6c5b
dneri <d.neri@zerynth.com>
2021-12-11 15:29:49 +0100

    changed weather station link to grafana

--- sin/resources/views/home.blade.php
+++ sin/resources/views/home.blade.php
@@ -27,7 +27,7 @@
               <div class="card-body">
                 <!-- <h3 class="card-title">Biblioteca</h3> -->
                 <p class="card-text">Stazione meteo situata nella zona delle scuole</p>
-                <a href="http://192.168.11.7:8081" class="btn btn-primary">Accedi</a>
+                <a href="http://localhost:3000/dashboard/snapshot/5m6NUuhY0wmBuHyaTZ04X4lk25p1dfwZ" class="btn btn-primary">Accedi</a>
               </div>
             </div>
           </div>
