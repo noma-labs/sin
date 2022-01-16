eaa2a17e4593d644d2e44a6110038194b7ab5b37
dneri <d.neri@zerynth.com>
2022-01-16 23:08:56 +0100

    laravel 7 with php7.4 passing tests

--- sin/routes/web.php
+++ sin/routes/web.php
@@ -253,7 +253,7 @@ Route::group(['prefix' => 'nomadelfia', 'namespace' => 'App\Nomadelfia\Controlle
 // ###################### DB SCUOLA ############################
 // ################################################################
 
-Route::mediaLibrary();
+//Route::mediaLibrary();
 
 Route::group(['prefix' => 'scuola', 'namespace' => 'App\Scuola\Controllers'], function () {
     Route::get('/', [ScuolaController::class, 'index'])->name('scuola');
--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 docker-compose up -d app
 #docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Actions --testdox
-#docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Unit --testdox
+docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Unit --testdox
 docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Feature --testdox
 #docker-compose exec app php ./vendor/bin/phpunit  --configuration /var/www/phpunit.xml --testsuite Http --testdox
