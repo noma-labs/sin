d492899c47e0fa0ac0b2230eb245547eb0ebfe54
dneri <d.neri@zerynth.com>
2022-02-22 21:26:54 +0100

    commented in refreesh

--- refresh.sh
+++ refresh.sh
@@ -4,6 +4,7 @@
 docker-compose exec app php artisan make:database db_scuola db_scuola
 docker-compose exec app php artisan make:database db_anagrafe db_anagrafe
 docker-compose exec app php artisan make:database db_patente db_patente
+docker-compose exec app php artisan make:database db_meccanica db_meccanica
 
 
 # THE ORDER IS IMPORTANT
@@ -12,7 +13,9 @@ docker-compose exec app php artisan migrate:fresh --path="database/migrations/an
 docker-compose exec app php artisan migrate:fresh --path="database/migrations/db_nomadelfia" --database=db_nomadelfia
 docker-compose exec app php artisan migrate:fresh --path="database/migrations/biblioteca" --database=db_biblioteca
 docker-compose exec app php artisan migrate:fresh --path="database/migrations/scuola" --database=db_scuola
-docker-compose exec app php artisan migrate # migrate the migrations outside the folder (lke the activity_log)
+#docker-compose exec app php artisan migrate:fresh --path="database/migrations/patente" --database=db_patente
+#docker-compose exec app php artisan migrate:fresh --path="database/migrations/officina" --database=db_officina
+docker-compose exec app php artisan migrate # migrate the migrations outside the folder (like the activity_log)
 
 # import dump of a db_nomadelfia
 DB_CONTAINER=$(docker-compose ps -q db)
