2988715cefcb3a8e07a2efba2aec24b451483e55
dneri <d.neri@zerynth.com>
2022-03-04 21:08:33 +0100

    refresh with sail

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-docker-compose up -d
+#docker-compose up -d
+#
+#sleep 5
+#
+#docker-compose ps
+
+alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
+sail up
 
 sleep 5
 
-docker-compose ps
+
+sail artisan make:database db_admsys
+sail artisan make:database db_nomadelfia
+sail artisan make:database archivio_biblioteca
+sail artisan make:database db_scuola db_scuola
+sail artisan make:database db_anagrafe db_anagrafe
+sail artisan make:database db_patente db_patente
+sail artisan make:database db_meccanica db_meccanica
