e4b3fd7644f787b48e8d2530360762271d21985b
dneri <d.neri@zerynth.com>
2022-08-18 08:33:38 +0200

    fix

--- sin/resources/views/nomadelfia/persone/edit_numero_elenco.blade.php
+++ sin/resources/views/nomadelfia/persone/edit_numero_elenco.blade.php
@@ -17,7 +17,7 @@
                             @if(count($assegnati) >0 )
                             <select class="form-control">
                                 @foreach ($assegnati as $p)
-                                    <option value="{{$p}}"> {{$p}} ({{$p->data_nascita}} {{$p->nome}} {{$p->cognome}})</option>
+                                    <option value="{{$p}}"> ({{$p->data_nascita}} {{$p->nome}} {{$p->cognome}})</option>
                                 @endforeach
                             </select>
                             @else
