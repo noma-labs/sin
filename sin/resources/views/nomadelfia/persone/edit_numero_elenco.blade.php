029b507b31c96312a99808a9aad482ce267ec34e
dneri <d.neri@zerynth.com>
2022-08-18 08:34:26 +0200

    fix position

--- sin/resources/views/nomadelfia/persone/edit_numero_elenco.blade.php
+++ sin/resources/views/nomadelfia/persone/edit_numero_elenco.blade.php
@@ -12,19 +12,19 @@
                       action="{{ route('nomadelfia.persone.nominativo.modifica', ['idPersona' =>$persona->id]) }}">
                     {{ csrf_field() }}
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-9">
                             <label for="exampleInputEmail1">precedenti</label>
                             @if(count($assegnati) >0 )
                             <select class="form-control">
                                 @foreach ($assegnati as $p)
-                                    <option value="{{$p}}"> ({{$p->data_nascita}} {{$p->nome}} {{$p->cognome}})</option>
+                                    <option value="{{$p}}"> {{$p->data_nascita}} {{$p->nome}} {{$p->cognome}}</option>
                                 @endforeach
                             </select>
                             @else
                                <p class="text-danger">Non ci sono numero elenco presenti</p>
                             @endif
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <label for="numeroElenco">Nuovo</label>
                             <input class="form-control" value="{{$propose}}" name="numero_elenco" readonly>
                         </div>
