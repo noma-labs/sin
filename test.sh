90932c6b77bafaa468ca0577d902fd426453e23c
dneri <d.neri@zerynth.com>
2022-09-09 16:18:15 +0200

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd sin
 
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    laravelsail/php74-composer:latest \
+    composer install --ignore-platform-reqs
+
+
 ./vendor/bin/sail  up -d
 
 ## Create the database
