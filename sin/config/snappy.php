736956e5689926995be19894b4c1e9f9c832c1c3
Davide N <davideneri18@gmail.com>
2022-09-27 15:43:27 +0200

    config souble quote

--- sin/config/snappy.php
+++ sin/config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => base_path('"vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf"'),
+        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
