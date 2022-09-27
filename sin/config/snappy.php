50bf8aabdc313bdb69f97d644856ec52423792f4
Davide N <davideneri18@gmail.com>
2022-09-27 15:34:45 +0200

    using wemersonjanuario windows executable to print etichette

--- sin/config/snappy.php
+++ sin/config/snappy.php
@@ -5,26 +5,17 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        // 'binary'  => '/usr/local/bin/wkhtmltopdf',
-        // 'binary' => base_path("vendor\h4cc\wkhtmltopdf-amd64\bin\wkhtmltopdf-amd64"),
-        // https://github.com/barryvdh/laravel-snappy/issues/60
-//        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
-        'binary' => base_path('vendor\h4cc\wkhtmltopdf-amd64\bin\wkhtmltopdf-amd64'),
-        // versione 0-12-5 scaricata dovrebb
-        //'binary' => base_path('"vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltox-0.12.5"'),
+        'binary' => base_path('"vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf"'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
-        // 'print-media-type' => true,
     ),
     'image' => array(
         'enabled' => true,
-        // 'binary'  => '/usr/local/bin/wkhtmltoimage',
         'binary' => base_path("vendor\h4cc\wkhtmltoimage-amd64\bin\wkhtmltoimage-amd64"),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
-
 
 );
