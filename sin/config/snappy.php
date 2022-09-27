4c945990f06b7d7d9e2cdd4edc410fe7a86fe46f
Davide N <davideneri18@gmail.com>
2022-09-27 15:47:44 +0200

    try doubel quote

--- sin/config/snappy.php
+++ sin/config/snappy.php
@@ -5,10 +5,11 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        'binary' => base_path('"vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf"'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
+
     ),
     'image' => array(
         'enabled' => true,
