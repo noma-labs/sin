9f0ac48f30d218b5206b16dcad200e904da0b412
Davide N <davideneri18@gmail.com>
2022-11-15 00:34:50 +0100

    hardcoded release version

--- sin/config/sentry.php
+++ sin/config/sentry.php
@@ -6,7 +6,7 @@ return [
 
     // capture release as git sha
 //    'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
-    'release' => trim(exec('git --git-dir ../../.git log --pretty="%h" -n1 HEAD')),
+    'release' => "ddad4acc",
 
     // When left empty or `null` the Laravel environment will be used
     'environment' => env('SENTRY_ENVIRONMENT'),
