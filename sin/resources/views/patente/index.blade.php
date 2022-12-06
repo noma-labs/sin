3807a346f36dcaaca265056a58c9b8b3302a2f3a
Davide N <davideneri18@gmail.com>
2022-12-06 11:37:17 +0100

    fix(patente.index): can middleware was wrong

--- sin/resources/views/patente/index.blade.php
+++ sin/resources/views/patente/index.blade.php
@@ -3,21 +3,22 @@
 @section('title', 'Patente')
 
 @section('navbar-link')
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="{{route('libri.ricerca')}}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Patenti
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="{{route('libri.ricerca')}}" id="navbarDropdown" role="button"
+       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Patenti
     </a>
 
     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-      <a class="dropdown-item" href="{{ route('patente.scadenze') }}">Scadenze patenti</a>
-      @can("patente.inserisci")
-      <a class="dropdown-item" href="{{ route('patente.inserimento') }}">Aggiungi patente</a>
-      @endcan
-      <a class="dropdown-item" href="{{ route('patente.ricerca') }}">Ricerca patente</a>
-  
+        <a class="dropdown-item" href="{{ route('patente.scadenze') }}">Scadenze patenti</a>
+        @can("scuolaguida.inserisci")
+        <a class="dropdown-item" href="{{ route('patente.inserimento') }}">Aggiungi patente</a>
+        @endcan
+        <a class="dropdown-item" href="{{ route('patente.ricerca') }}">Ricerca patente</a>
+
     </div>
-  </li>
-  <li class="nav-item">
-    <a  class="nav-link" href="{{ route('patente.elenchi') }}">Elenchi patenti</a>
-  </li>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('patente.elenchi') }}">Elenchi patenti</a>
+</li>
 @append
