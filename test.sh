366206b3531d8add42e31d4b5bc93f951203f962
Davide N <davideneri18@gmail.com>
2023-03-22 23:46:09 +0100

    test.sh with up command

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#./vendor/bin/sail  up -d
+./vendor/bin/sail  up -d
 #./vendor/bin/sail  php artisan key:generate --env=testing
-#./vendor/bin/sail  php artisan config:clear
+./vendor/bin/sail  php artisan config:clear
 ./vendor/bin/sail  test
