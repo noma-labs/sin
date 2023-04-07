3eb172ef479cea9f06741809e51a3542eab4c4a7
Davide N <davideneri18@gmail.com>
2023-04-07 17:26:28 +0200

    test: move tests into UNit into the specific folder

---
+++ tests/Popolazione/Unit/EsSpiritualiTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Domain\Nomadelfia\EserciziSpirituali\Models\EserciziSpirituali;
+use Domain\Nomadelfia\Persona\Models\Persona;
+
+use App\Nomadelfia\Exceptions\EsSpiritualeNotActive;
+
+it("testAggiungiPersona", function () {
+    $esercizi = EserciziSpirituali::factory()->create();
+    $persona = Persona::factory()->maggiorenne()->maschio()->create();
+    $esercizi->aggiungiPersona($persona);
+    $this->assertEquals($esercizi->persone->count(), 1);
+    //$this->assertEquals($persona->eserciziSpiritualiAttuale()->count(), 1);
+});
+
+it("testEsSpiritualeNotActive", function () {
+    $esercizi = EserciziSpirituali::factory()->disattivo()->create();
+    $persona = Persona::factory()->maggiorenne()->maschio()->create();
+    $this->expectException(EsSpiritualeNotActive::class);
+    $esercizi->aggiungiPersona($persona);
+});
+
+it("testAssegnaResponsabile", function () {
+    $esercizi = EserciziSpirituali::factory()->create();
+    $resp = Persona::factory()->maggiorenne()->maschio()->create();
+    $esercizi->assegnaResponsabile($resp);
+    $this->assertEquals($esercizi->responsabile->id, $resp->id);
+});
+
---
+++ tests/Popolazione/Unit/IncarichiTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit;
+
+use Domain\Nomadelfia\Incarico\Models\Incarico;
+use Domain\Nomadelfia\Persona\Models\Persona;
+use Tests\CreatesApplication;
+use Tests\MigrateFreshDB;
+use Tests\TestCase;
+use Carbon;
+
+
+it("testIncarich", function () {
+    $persona = Persona::factory()->cinquantenne()->maschio()->create();
+    $incarico = Incarico::factory()->create();
+
+    $this->assertEquals(0, $incarico->lavoratori()->count());
+    $this->assertEquals(0, $incarico->lavoratoriAttuali()->count());
+    $this->assertEquals(0, $incarico->lavoratoriStorici()->count());
+    $this->assertEquals(0, $persona->aziendeAttuali()->count());
+    $this->assertEquals(0, $persona->aziendeStorico()->count());
+    $this->assertEquals(0, $persona->incarichiAttuali()->count());
+    $this->assertEquals(0, $persona->incarichiStorico()->count());
+
+
+    $data_inizio = Carbon::now()->addYears(5);
+    $persona->assegnaLavoratoreIncarico($incarico, $data_inizio);
+    $this->assertEquals(1, $incarico->lavoratoriAttuali()->count());
+
+    $this->assertEquals(1, $persona->incarichiAttuali()->count());
+    $this->assertEquals(0, $persona->aziendeAttuali()->count());
+});
+
+it("it_get_the_most_busy_people", function () {
+    $busyPeaple = Persona::factory()->cinquantenne()->maschio()->create();
+
+    $num = 10;
+    for ($i = 1; $i <= $num; $i++) {
+        $incarico = Incarico::factory()->create();
+        $busyPeaple->assegnaLavoratoreIncarico($incarico, Carbon::now());
+    }
+
+    $p = Incarico::getBusyPeople();
+    $this->assertEquals($busyPeaple->id, $p[0]->id);
+    $this->assertEquals(10, $p[0]->count);
+
+});
