d3b2bd0304952abe8cb5458db5ca64087750d2de
Davide N <davideneri18@gmail.com>
2023-04-17 14:38:09 +0200

    test: exit test on first fail

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 ./vendor/bin/sail  up -d
 #./vendor/bin/sail  php artisan key:generate --env=testing
 ./vendor/bin/sail  php artisan config:clear
-./vendor/bin/sail  test
+./vendor/bin/sail  test --stop-on-error
