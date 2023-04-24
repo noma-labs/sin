c2bac94931a8660572620c6876869390ae80d97c
dido18 <dido18@users.noreply.github.com>
2023-04-24 22:29:29 +0000

    Fix styling with Laravel Pint

--- src/Domain/Nomadelfia/PopolazioneNomadelfia/Actions/LogUscitaNomadelfiaAsActivityAction.php
+++ src/Domain/Nomadelfia/PopolazioneNomadelfia/Actions/LogUscitaNomadelfiaAsActivityAction.php
@@ -11,13 +11,13 @@ class LogUscitaNomadelfiaAsActivityAction
         activity('nomadelfia')
             ->performedOn($uscitaData->persona)
             ->withProperties([
-                    'nominativo' => $uscitaData->persona->nominativo,
-                    'luogo_nascita' => $uscitaData->persona->provincia_nascita,
-                    'data_nascita' => $uscitaData->persona->data_nascita,
-                    'data_entrata' => $uscitaData->data_entrata,
-                    'data_uscita' => $uscitaData->data_uscita,
-                    'numero_elenco' => $uscitaData->persona->numero_elenco,
-                ]
+                'nominativo' => $uscitaData->persona->nominativo,
+                'luogo_nascita' => $uscitaData->persona->provincia_nascita,
+                'data_nascita' => $uscitaData->persona->data_nascita,
+                'data_entrata' => $uscitaData->data_entrata,
+                'data_uscita' => $uscitaData->data_uscita,
+                'numero_elenco' => $uscitaData->persona->numero_elenco,
+            ]
             )
             ->setEvent('popolazione.uscita')
             ->log('Uscito da Nomadelfia in data :properties.data_uscita');
