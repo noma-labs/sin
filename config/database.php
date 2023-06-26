703e5fc4100859018d65023bd8e9b4266017cdfc
nomadelfia <135833379+nomadelfia@users.noreply.github.com>
2023-06-26 11:07:25 +0200

    Deleted db_anagrafe

--- config/database.php
+++ config/database.php
@@ -98,22 +98,6 @@ return [
                 'setDumpBinaryPath' => 'C:\xampp\mysql\bin',
             ],
         ],
-        'db_anagrafe' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'db_anagrafe'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'root'),
-            'unix_socket' => env('DB_SOCKET', ''),
-
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'dump' => [
-                'setDumpBinaryPath' => 'C:\xampp\mysql\bin',
-            ],
-        ],
         'db_officina' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
