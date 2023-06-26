c81ac04a8a393a251362fa04439235cf3f524940
nomadelfia <135833379+nomadelfia@users.noreply.github.com>
2023-06-26 11:13:12 +0200

    Add db_foto and db_noma_iot

--- config/database.php
+++ config/database.php
@@ -200,6 +200,38 @@ return [
                 'setDumpBinaryPath' => 'C:\xampp\mysql\bin',
             ],
         ],
+        'db_foto' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'db_foto'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
+            'unix_socket' => env('DB_SOCKET', ''),
+
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+            'dump' => [
+                'setDumpBinaryPath' => 'C:\xampp\mysql\bin',
+            ],
+        ],
+         'db_noma_iot' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'db_noma_iot'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
+            'unix_socket' => env('DB_SOCKET', ''),
+
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+            'dump' => [
+                'setDumpBinaryPath' => 'C:\xampp\mysql\bin',
+            ],
+        ],
 
     ],
 
