4979c707649a074841358430e77ed32e532b0729
nomadelfia <135833379+nomadelfia@users.noreply.github.com>
2023-06-28 09:59:49 +0200

    Improve aggiornamento anagrafe (#25)
    * refactor view aggiornamento anagrafe
    
    ---------
    
    Co-authored-by: noma-dev <tv@nomadelfia.com>
    Co-authored-by: nomadelfia <nomadelfia@users.noreply.github.com>

--- src/App/Sin/Nomadelfia/PopolazioneNomadelfia/Controllers/AggiornamentoAnagrafeController.php
+++ src/App/Sin/Nomadelfia/PopolazioneNomadelfia/Controllers/AggiornamentoAnagrafeController.php
@@ -4,15 +4,13 @@ namespace App\Nomadelfia\PopolazioneNomadelfia\Controllers;
 
 use App\Core\Controllers\BaseController as CoreBaseController;
 use Domain\Nomadelfia\AggiornamentoAnagrafe\Models\AggiornamentoAnagrafe;
-use Spatie\Activitylog\Models\Activity;
 
 class AggiornamentoAnagrafeController extends CoreBaseController
 {
     public function index()
     {
-        $entrati = AggiornamentoAnagrafe::entrati(); // Activity::inLog('nomadelfia')->ForEvent('popolazione.entrata')->orderBy('created_at', 'DESC')->take(20)->get();
-        $usciti = Activity::inLog('nomadelfia')->ForEvent('popolazione.uscita')->orderBy('created_at', 'DESC')->take(20)->get();
+        $activity = AggiornamentoAnagrafe::orderBy('created_at', 'DESC')->take(20)->get();
 
-        return view('nomadelfia.activity.popolazione', compact('entrati', 'usciti'));
+        return view('nomadelfia.aggiornamento-anagrafe.index', compact('activity'));
     }
 }
