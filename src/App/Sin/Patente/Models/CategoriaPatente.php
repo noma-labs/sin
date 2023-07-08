3fb8f06278d417cb21f92fc0a0c6ab7896fc10ca
nomadelfia <nomadelfia@users.noreply.github.com>
2023-07-08 14:19:24 +0000

    Fix styling with Laravel Pint

--- src/App/Sin/Patente/Models/CategoriaPatente.php
+++ src/App/Sin/Patente/Models/CategoriaPatente.php
@@ -30,8 +30,8 @@ class CategoriaPatente extends Model
     /**
      * Scope a query to only include categorie of a given name (A,B,C,D,DE,...)
      *
-     * @param Builder $query
-     * @param mixed $type
+     * @param  Builder  $query
+     * @param  mixed  $type
      * @return Builder
      */
     public function scopeDalNome($query, $categoria)
@@ -67,7 +67,7 @@ class CategoriaPatente extends Model
     /**
      * Ritorna le patenti che scadono entro $days giorni
      *
-     * @param int $days :numero di giorni entro il quale le patenti scadono.
+     * @param  int  $days :numero di giorni entro il quale le patenti scadono.
      *
      * @author Davide Neri
      */
