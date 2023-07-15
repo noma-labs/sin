6f0ec3d229694ba76cba191b10b31fe732713a17
nomadelfia <nomadelfia@users.noreply.github.com>
2023-07-15 13:48:56 +0000

    Fix styling with Laravel Pint

--- src/App/Console/Commands/ConvertExifCommand.php
+++ src/App/Console/Commands/ConvertExifCommand.php
@@ -32,10 +32,10 @@ class ConvertExifCommand extends Command
     // exiftool -r -n -a -struct -progress1000 -G1 -file:all  -exif:all -iptc:all -ImageDataHash -json>2000.json 2000
     public function handle()
     {
-//        $files = ['2000.json', '2001.json', '2002.json', '2003.json', '2004.json', '2005.json', '2006.json', '2007.json', '2008.json', '2009.json', '2010.json', '2011.json', '2013.json', '2014.json'];
+        //        $files = ['2000.json', '2001.json', '2002.json', '2003.json', '2004.json', '2005.json', '2006.json', '2007.json', '2008.json', '2009.json', '2010.json', '2011.json', '2013.json', '2014.json'];
         $files = ['2015.json', '2016.json', '2017.json', '2018.json', '2019.json', '2020.json', '2021.json', '2022.json', '2023.json'];
         foreach ($files as $f) {
-            $photos = json_decode(file_get_contents(storage_path() . '/' . $f), true);
+            $photos = json_decode(file_get_contents(storage_path().'/'.$f), true);
 
             $raw = collect([]);
             foreach ($photos as $photo) {
@@ -46,14 +46,16 @@ class ConvertExifCommand extends Command
                 $exif->directory = $photo['System:Directory'];
                 $exif->fileSize = $photo['System:FileSize'];
 
-                if (!isset($photo['File:FileType'])) {
-                    $this->warn("File type missing " . $exif->sourceFile);
+                if (! isset($photo['File:FileType'])) {
+                    $this->warn('File type missing '.$exif->sourceFile);
+
                     continue;
                 }
                 $exif->fileType = $photo['File:FileType'];
                 $exif->fileExtension = $photo['File:FileTypeExtension'];
                 if ($exif->fileType == 'MPEG') {
-                    $this->warn("MPEG video skipped: " . $exif->sourceFile);
+                    $this->warn('MPEG video skipped: '.$exif->sourceFile);
+
                     continue;
                 }
                 if (isset($photo['File:ImageWidth'])) {
@@ -64,8 +66,9 @@ class ConvertExifCommand extends Command
                 }
                 $exif->folderTitle = Str::of($exif->directory)->basename();
 
-                if (!isset($photo['File:ImageDataHash'])) {
-                    $this->warn("Photo with missing File:ImageDataHash " . $exif->sourceFile);
+                if (! isset($photo['File:ImageDataHash'])) {
+                    $this->warn('Photo with missing File:ImageDataHash '.$exif->sourceFile);
+
                     continue;
                 }
                 $exif->sha = $photo['File:ImageDataHash'];
