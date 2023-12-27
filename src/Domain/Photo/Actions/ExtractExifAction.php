5634c011fa8b0ce5e4a6d9ebbc5ae2c4d71ccf98
Davide N <davideneri18@gmail.com>
2023-12-27 12:07:51 +0100

    xmp info

--- src/Domain/Photo/Actions/ExtractExifAction.php
+++ src/Domain/Photo/Actions/ExtractExifAction.php
@@ -16,6 +16,7 @@ class ExtractExifAction
             ->enableStructuredInformation()
             ->flatGroup1Tag()
             ->extractFileInformation()
+            ->extractXMPInformation()
             ->extractExifInformation()
             ->extractIPTCInformation()
             ->extractHashOfTheImage()
