357ae024febcb01c7c4df98c4ff77bfd57f9dc00
Davide N <davideneri18@gmail.com>
2024-01-08 21:50:07 +0100

    revert migration

--- database/migrations/foto/2023_07_07_100131_create_photos_table.up.sql
+++ database/migrations/foto/2023_07_07_100131_create_photos_table.up.sql
@@ -28,13 +28,14 @@ group by folder_title;
 
 CREATE TABLE `foto_persone` (
     `photo_id` varchar(255) NOT NULL,
+    `persona_id` bigint(10) DEFAULT NULL,
     `persona_nome` varchar(255)  DEFAULT NULL,
     `created_at` datetime DEFAULT CURRENT_TIMESTAMP(),
     `updated_at` datetime DEFAULT CURRENT_TIMESTAMP()
 ) ENGINE=InnoDB;
 
 ALTER TABLE `foto_persone`
-    ADD UNIQUE KEY (`photo_id`,`persona_nome`);
+    ADD UNIQUE KEY (`photo_id`,`persona_id`);
 
 -- ALTER TABLE `foto_persone`
 --     ADD CONSTRAINT `foto_persone_ibfk_1` FOREIGN KEY (`photo_id`) REFERENCES `photos` (`uid`);
