8ac651a457ba7248b04d036a2ae4703a22627d5b
Davide N <davideneri18@gmail.com>
2024-02-22 10:27:00 +0100

    rtn: add migration

--- config/database.php
+++ config/database.php
@@ -191,6 +191,19 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+        'db_rtn' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'db_rtn'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
     ],
 
