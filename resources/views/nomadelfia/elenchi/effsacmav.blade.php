c6fe20b05bc199bb16909d6fecd1e54f57d5f7e8
Davide N <davideneri18@gmail.com>
2024-03-11 22:00:44 +0100

    prettiere allview/nomadelfia

--- resources/views/nomadelfia/elenchi/effsacmav.blade.php
+++ resources/views/nomadelfia/elenchi/effsacmav.blade.php
@@ -1,28 +1,26 @@
-
 <div class="row">
     <div class="col-md-3">
-      <h2>Effettivi Uomini {{count($effettivi->uomini)}}</h2>
-        @foreach($effettivi->uomini as $uomo)
-           <div>{{$uomo->nominativo}}</div>
+        <h2>Effettivi Uomini {{ count($effettivi->uomini) }}</h2>
+        @foreach ($effettivi->uomini as $uomo)
+            <div>{{ $uomo->nominativo }}</div>
         @endforeach
     </div>
     <div class="col-md-3">
-      <h2>Effettivi Donne {{count($effettivi->donne)}}</h2>
-        @foreach($effettivi->donne as $donna)
-         <div>{{$donna->nominativo}}</div>  
+        <h2>Effettivi Donne {{ count($effettivi->donne) }}</h2>
+        @foreach ($effettivi->donne as $donna)
+            <div>{{ $donna->nominativo }}</div>
         @endforeach
     </div>
     <div class="col-md-3">
-      <h2>Sacerdoti {{count($sacerdoti  )}}</h2>
-       @forelse($sacerdoti as $sacerdote)
-             <div>{{$sacerdote->nominativo}}</div>  
-        @endforeach
+        <h2>Sacerdoti {{ count($sacerdoti) }}</h2>
+        @forelse ($sacerdoti as $sacerdote)
+            <div>{{ $sacerdote->nominativo }}</div>
+        @endforelse
     </div>
-     <div class="col-md-3">
-      <h2>Mamme Di vocazione {{count($mvocazione)}}</h2>
-       @forelse($mvocazione as $mammavoc)
-             <div>{{$mammavoc->nominativo}}</div>  
-        @endforeach
+    <div class="col-md-3">
+        <h2>Mamme Di vocazione {{ count($mvocazione) }}</h2>
+        @forelse ($mvocazione as $mammavoc)
+            <div>{{ $mammavoc->nominativo }}</div>
+        @endforelse
     </div>
-  
 </div>
