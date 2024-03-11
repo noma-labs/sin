56e65b96be3b7d979dfc537b4dd719974a96b460
Davide N <davideneri18@gmail.com>
2024-03-11 21:54:22 +0100

    prettiere nomadeflia

--- resources/views/nomadelfia/gruppifamiliari/edit.blade.php
+++ resources/views/nomadelfia/gruppifamiliari/edit.blade.php
@@ -133,70 +133,84 @@
                 <div class="card-body">
                     <h4 class="card-title">Persone divise per famiglie</h4>
                     <ul class="list-group list-group-flush">
-
-                        @foreach($single as $persona)
-                        <li class="list-group-item">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="font-weight-bold mt-2">
-                                            @include("nomadelfia.templates.persona", ['persona' => $persona])
-                                    </div>
-                                </div>
-                                <div class="col-md-4 offset-md-2">
-                                </div>
-                            </div>
-
-                        </li>
-                        @endforeach
-
-                        @foreach($famiglie as $famiglia_id => $componenti)
-                        <li class="list-group-item">
+                        @foreach ($single as $persona)
+                            <li class="list-group-item">
                                 <div class="row">
-                                    @if ($famiglia_id === "")
-                                        <p class="text-danger">Persone senza famiglia </p>
-                                    @else
                                     <div class="col-md-6">
                                         <div class="font-weight-bold mt-2">
-                                             Fam.
-                                             <a  href="{{route('nomadelfia.famiglia.dettaglio',['id'=>$famiglia_id])}}">
-                                                    {{Domain\Nomadelfia\Famiglia\Models\Famiglia::find($famiglia_id)->nome_famiglia}}
-                                             </a>({{count($componenti)}})
-
+                                            @include("nomadelfia.templates.persona", ["persona" => $persona])
                                         </div>
-                                        <ul>
-                                        @foreach($componenti as $componente)
-                                        <li> @year($componente->data_nascita)
-                                            <a href="{{route('nomadelfia.persone.dettaglio',['idPersona'=>$componente->persona_id])}}">
-                                            {{$componente->nominativo}}</a>
-                                        </li>
-                                        @endforeach
-                                        </ul>
                                     </div>
-                                    <div class="col-md-4 offset-md-2">
-                                        @include('nomadelfia.templates.spostaFamigliaNuovoGruppo', ['famiglia_id'=>$famiglia_id, 'componenti'=>$componenti, "gruppo_id"=>$gruppo->id])
-                                    </div>
+                                    <div class="col-md-4 offset-md-2"></div>
                                 </div>
-                                @endif
                             </li>
                         @endforeach
-                    <ul>
+
+                        @foreach ($famiglie as $famiglia_id => $componenti)
+                            <li class="list-group-item">
+                                <div class="row">
+                                    @if ($famiglia_id === "")
+                                        <p class="text-danger">
+                                            Persone senza famiglia
+                                        </p>
+                                    @else
+                                        <div class="col-md-6">
+                                            <div class="font-weight-bold mt-2">
+                                                Fam.
+                                                <a
+                                                    href="{{ route("nomadelfia.famiglia.dettaglio", ["id" => $famiglia_id]) }}"
+                                                >
+                                                    {{ Domain\Nomadelfia\Famiglia\Models\Famiglia::find($famiglia_id)->nome_famiglia }}
+                                                </a>
+                                                ({{ count($componenti) }})
+                                            </div>
+                                            <ul>
+                                                @foreach ($componenti as $componente)
+                                                    <li>
+                                                        @year($componente->data_nascita)
+                                                        <a
+                                                            href="{{ route("nomadelfia.persone.dettaglio", ["idPersona" => $componente->persona_id]) }}"
+                                                        >
+                                                            {{ $componente->nominativo }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                        <div class="col-md-4 offset-md-2">
+                                            @include("nomadelfia.templates.spostaFamigliaNuovoGruppo", ["famiglia_id" => $famiglia_id, "componenti" => $componenti, "gruppo_id" => $gruppo->id])
+                                        </div>
+                                    @endif
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
 
         <div class="col-md-6">
             <div class="card">
-             <div class="card-body">
-             <h4 class="card-title">Persone <span class="badge badge-primary">{{$gruppo->personeAttuale->count()}}</span></h4>
-                <ul class="list-group list-group-flush">
-                @foreach($gruppo->personeAttuale as $persona)
-                     <li class="list-group-item"> @year($persona->data_nascita)
-                            @include("nomadelfia.templates.persona", ['persona' => $persona])
-                            (@diffYears($persona->data_nascita) anni)</li>
-                @endforeach
-                </ul>
-            </div>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        Persone
+                        <span class="badge badge-primary">
+                            {{ $gruppo->personeAttuale->count() }}
+                        </span>
+                    </h4>
+                    <ul class="list-group list-group-flush">
+                        @foreach ($gruppo->personeAttuale as $persona)
+                            <li class="list-group-item">
+                                @year($persona->data_nascita)
+                                @include("nomadelfia.templates.persona", ["persona" => $persona])
+                                (
+                                @diffYears($persona->data_nascita)
+                                anni)
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
-     </div>
+    </div>
 @endsection
