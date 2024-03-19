281b6f75ae6cf851f02d4cf83196226043cc5dcb
Davide N <davideneri18@gmail.com>
2024-03-20 00:06:01 +0100

    make linter happy

--- resources/views/livewire/search-persona.blade.php
+++ resources/views/livewire/search-persona.blade.php
@@ -1,25 +1,32 @@
 <div>
-    <p> Search: {{$searchTerm}}</p>
-<input type="text" wire:model.live.debounce.100ms="searchTerm">
+    <p>Search: {{ $searchTerm }}</p>
+    <input type="text" wire:model.live.debounce.100ms="searchTerm" />
 
+    Persone:
+    <ul>
+        @foreach ($people as $person)
+            <li wire:key="{{ $person->id }}">
+                {{ $person->nominativo }}
+                <button type="button" wire:click="add({{ $person->id }})">
+                    Add
+                </button>
+            </li>
+        @endforeach
+    </ul>
 
-Persone:
-<ul>
-@foreach($people as $person)
-    <li wire:key="{{ $person->id }}">{{ $person->nominativo}}
-        <button type="button" wire:click="add({{$person->id}})">Add</button>
-    </li>
-@endforeach
-</ul>
-
-
-Selected :
-<ul>
-@foreach($selected as $s)
-    <li wire:key="{{ $s->id }}">{{ $s->nominativo}}
-        <button type="button" wire:click="remove({{$s->id}})" wire:confirm="Are you sure">Remove</button>
-    </li>
-@endforeach
-</ul>
-
+    Selected :
+    <ul>
+        @foreach ($selected as $s)
+            <li wire:key="{{ $s->id }}">
+                {{ $s->nominativo }}
+                <button
+                    type="button"
+                    wire:click="remove({{ $s->id }})"
+                    wire:confirm="Are you sure"
+                >
+                    Remove
+                </button>
+            </li>
+        @endforeach
+    </ul>
 </div>
--- src/App/Livewire/SearchPersona.php
+++ src/App/Livewire/SearchPersona.php
@@ -8,35 +8,38 @@ use Livewire\Component;
 class SearchPersona extends Component
 {
     public $searchTerm = '';
+
     public $placehodler = '<inserisci nominativo>';
 
     public $people = [];
 
     public $selected = [];
 
-    public function mount() {
+    public function mount()
+    {
         // $this->people = Persona::orderBy('nominativo')->limit(10)->get();
         // setup component: get the data from db and set the properties
-     }
+    }
 
     public function render()
     {
         return view('livewire.search-persona');
     }
 
-
     public function updatedSearchTerm($value)
     {
         $this->reset('people');
-        $this->people = Persona::where('nominativo', 'LIKE', "$value%") ->orderBy('nominativo') ->get();
+        $this->people = Persona::where('nominativo', 'LIKE', "$value%")->orderBy('nominativo')->get();
     }
 
-    public function add($personID){
+    public function add($personID)
+    {
         $this->selected[] = Persona::find($personID);
     }
 
-    public function remove($personID){
-        $this->selected = array_filter($this->selected, function($persona) use ($personID) {
+    public function remove($personID)
+    {
+        $this->selected = array_filter($this->selected, function ($persona) use ($personID) {
             return $persona->id !== $personID;
         });
     }
