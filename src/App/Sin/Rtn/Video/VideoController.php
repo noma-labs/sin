26e94618f54c035c990a0683003c46898bd6e541
Davide N <davideneri18@gmail.com>
2024-03-25 23:04:13 +0100

    linter happy

--- src/App/Sin/Rtn/Video/VideoController.php
+++ src/App/Sin/Rtn/Video/VideoController.php
@@ -4,18 +4,18 @@ namespace App\Rtn\Video;
 
 use App\Core\Controllers\BaseController as CoreBaseController;
 use Exception;
-use Pest\Panic;
 
 class VideoController extends CoreBaseController
 {
     public function index()
     {
         $countByYear = Video::byYear()->get();
+
         return view('rtn.video.create', compact('countByYear'));
     }
 
     public function create()
     {
-        throw new Exception("not implemented yet");
+        throw new Exception('not implemented yet');
     }
 }
