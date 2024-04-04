48c9752db2abd4a5b78e99b101cbf7b3d0322989
Davide N <davideneri18@gmail.com>
2024-04-04 23:26:30 +0200

    using favicon instead of img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8" />
         <title>@yield("title")</title>
-        <link rel="icon" href="/images/noma.png" type="image/png" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
