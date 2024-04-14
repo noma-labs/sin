e0acabd13c9a5321429d2a86b515f9a0ebba92e2
Davide N <davideneri18@gmail.com>
2024-04-14 23:08:23 +0200

    initial form

--- resources/views/rtn/video/create.blade.php
+++ resources/views/rtn/video/create.blade.php
@@ -3,24 +3,173 @@
 @section("title", "Inserisci Video")
 
 @section("archivio")
-    <h1>Archivio professionale</h1>
-
+    @include("partials.header", ["title" => "Aggiungi Video Archivio Professionale"])
     <form>
-        <div class="form-group row">
-          <label for="inputDispositivo" class="col-sm-2 col-form-label">Dispositivo</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputDispositivo" placeholder="--inserisci dispositivo--">
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="inputCategoria" class="col-sm-2 col-form-label">Categoria</label>
-          <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputCategoria" placeholder="--inserisci categoria--">
-          </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputDispositivo" class="col-sm-2 col-form-label">
+                    Dispositivo
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputDispositivo"
+                        placeholder="--inserisci dispositivo--"
+                    />
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputCategoria" class="col-sm-2 col-form-label">
+                    Categoria
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputCategoria"
+                        placeholder="--inserisci categoria--"
+                    />
+                </div>
+            </div>
         </div>
 
-        <div class="form-group row">
-            <label for="inputPersona" class="col-sm-2 col-form-label">Persona</label>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label
+                    for="inputCategoriaEvento"
+                    class="col-sm-2 col-form-label"
+                >
+                    CategoriaEvento
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputCategoriaEvento"
+                        placeholder="--inserisci categoriaevento"
+                    />
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputRecord" class="col-sm-2 col-form-label">
+                    Record
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputRecord"
+                        placeholder="--inserisci categoria--"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputInizioMinuti" class="col-sm-2 col-form-label">
+                    Inizio Minuti
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputInizioMinuti"
+                        placeholder="--inserisci categoriaevento"
+                    />
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputFineMinuti" class="col-sm-2 col-form-label">
+                    Fine Minuti
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputFineMinuti"
+                        placeholder="--inserisci categoria--"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputInizioMinuti" class="col-sm-2 col-form-label">
+                    Data Registrazione
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputInizioMinuti"
+                        placeholder="--inserisci categoriaevento"
+                    />
+                </div>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputFineMinuti" class="col-sm-2 col-form-label">
+                    Data Ultima trasmissione
+                </label>
+                <div class="col-sm-10">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="inputFineMinuti"
+                        placeholder="--inserisci categoria--"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputPersona" class="col-sm-2 col-form-label">
+                Località
+            </label>
+            <div class="col-sm-10">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="inputFineMinuti"
+                    placeholder="--inserisci categoria--"
+                />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputPersona" class="col-sm-2 col-form-label">
+                Nome Cartella
+            </label>
+            <div class="col-sm-10">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="inputFineMinuti"
+                    placeholder="--inserisci categoria--"
+                />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputPersona" class="col-sm-2 col-form-label">
+                Argomento
+            </label>
+            <div class="col-sm-10">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="inputFineMinuti"
+                    placeholder="--inserisci categoria--"
+                />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputPersona" class="col-sm-2 col-form-label">
+                Persona
+            </label>
             <div class="col-sm-10">
                 <livewire:search-persona
                     inputName="persona_id"
@@ -28,13 +177,12 @@
                     noResultsMessage="Nessun risultato"
                 />
             </div>
-          </div>
-
-
-        <div class="form-group row">
-          <div class="col-sm-10">
-            <button type="submit" class="btn btn-primary">Inserisci</button>
-          </div>
         </div>
-      </form>
+
+        <div class="form-group">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Inserisci</button>
+            </div>
+        </div>
+    </form>
 @endsection
