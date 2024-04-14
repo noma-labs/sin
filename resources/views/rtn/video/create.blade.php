75162a1d5e24886a1b7af19b1353daeb06422440
Davide N <davideneri18@gmail.com>
2024-04-14 22:59:16 +0200

    first version

--- resources/views/rtn/video/create.blade.php
+++ resources/views/rtn/video/create.blade.php
@@ -5,18 +5,36 @@
 @section("archivio")
     <h1>Archivio professionale</h1>
 
-    <livewire:search-persona
-        inputName="persona_id"
-        placeholder="Cerca persona"
-        noResultsMessage="Nessun risultato"
-    />
+    <form>
+        <div class="form-group row">
+          <label for="inputDispositivo" class="col-sm-2 col-form-label">Dispositivo</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="inputDispositivo" placeholder="--inserisci dispositivo--">
+          </div>
+        </div>
+        <div class="form-group row">
+          <label for="inputCategoria" class="col-sm-2 col-form-label">Categoria</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="inputCategoria" placeholder="--inserisci categoria--">
+          </div>
+        </div>
 
-    <ul>
-        @foreach ($countByYear as $c)
-            <li>
-                {{ $c->year }} {{ $c->month }}
-                <span class="badge badge-secondary">{{ $c->count }}</span>
-            </li>
-        @endforeach
-    </ul>
+        <div class="form-group row">
+            <label for="inputPersona" class="col-sm-2 col-form-label">Persona</label>
+            <div class="col-sm-10">
+                <livewire:search-persona
+                    inputName="persona_id"
+                    placeholder="Cerca persona"
+                    noResultsMessage="Nessun risultato"
+                />
+            </div>
+          </div>
+
+
+        <div class="form-group row">
+          <div class="col-sm-10">
+            <button type="submit" class="btn btn-primary">Inserisci</button>
+          </div>
+        </div>
+      </form>
 @endsection
