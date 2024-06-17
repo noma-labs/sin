87c4c5e92d04f5b82daacf7a33dba1f6d09bf542
Davide N <davideneri18@gmail.com>
2024-06-17 21:29:16 +0200

    instace of

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
@@ -12,6 +13,7 @@ return RectorConfig::configure()
     ])
     ->withPreparedSets(deadCode: true, typeDeclarations: true)
     ->withSets([
+        SetList::INSTANCEOF,
         LaravelSetList::LARAVEL_90,
         LaravelSetList::LARAVEL_100,
     ]);
