2bac28407067073bb921783e71d5ea9721135043
Davide N <davideneri18@gmail.com>
2024-07-13 15:31:01 +0200

    ignore

--- tests/Scuola/Http/ScuolaTest.php
+++ tests/Scuola/Http/ScuolaTest.php
@@ -43,7 +43,7 @@ it('can list classi in anno scolastico', function (): void {
     $this->assertCount(1, $c->alunni()->get());
 
     login();
-    $this->get(action([ScuolaController::class, 'index'], ['id' => $a->id]))
+    $this->get(action([ScuolaController::class, 'show'], ['id' => $a->id]))
         ->assertSuccessful();
 
 });
