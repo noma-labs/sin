a72d961e2e27ff6b9c4b09ce98e15d8df071815e
Davide <davideneri18@gmail.com>
2024-07-13 15:42:25 +0200

    fix(test): if  `index` not exists (#102)
    * ignore
    
    * fix

--- tests/Scuola/Http/ScuolaTest.php
+++ tests/Scuola/Http/ScuolaTest.php
@@ -43,7 +43,7 @@ it('can list classi in anno scolastico', function (): void {
     $this->assertCount(1, $c->alunni()->get());
 
     login();
-    $this->get(action([ScuolaController::class, 'index'], ['id' => $a->id]))
+    $this->get(action([ScuolaController::class, 'show'], ['id' => $a->id]))
         ->assertSuccessful();
 
 });
@@ -66,7 +66,7 @@ it('can delete classe', function (): void {
 
     $this->delete(action([ClassiController::class, 'delete'], ['id' => $c->id]));
 
-    $this->get(action([ScuolaController::class, 'index'], ['id' => $a->id]))
+    $this->get(action([ScuolaController::class, 'show'], ['id' => $a->id]))
         ->assertSuccessful()
         ->assertDontSee($s->nominativo);
 });
