a01de010fa9534a497e05d230c08a5f72790b5f3
Davide N <davideneri18@gmail.com>
2024-12-08 21:59:37 +0100

    fix tests

--- tests/Scuola/Http/ClasseTest.php
+++ tests/Scuola/Http/ClasseTest.php
@@ -37,7 +37,7 @@ it('can delete classe', function (): void {
 });
 
 it('update the tipo of a classe', function (): void {
-    $a = Anno::createAnno(2000);
+    $a = Anno::createAnno(2002);
     $c = $a->aggiungiClasse(ClasseTipo::first());
     $s = Studente::factory()->minorenne()->maschio()->create();
     $c->aggiungiAlunno($s, Carbon::now());
