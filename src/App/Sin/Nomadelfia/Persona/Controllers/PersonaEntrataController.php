1dcbe17c7c009db4bbf51bca308465ace2a5dbf1
Davide N <davideneri18@gmail.com>
2025-01-07 07:48:46 +0100

    nochange

---
+++ src/App/Sin/Nomadelfia/Persona/Controllers/PersonaEntrataController.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Nomadelfia\Persona\Controllers;
+
+use App\Nomadelfia\PopolazioneNomadelfia\Requests\EntrataPersonaRequest;
+use Carbon\Carbon;
+use Domain\Nomadelfia\Famiglia\Models\Famiglia;
+use Domain\Nomadelfia\GruppoFamiliare\Models\GruppoFamiliare;
+use Domain\Nomadelfia\Persona\Models\Persona;
+use Domain\Nomadelfia\PopolazioneNomadelfia\Actions\EntrataDallaNascitaAction;
+use Domain\Nomadelfia\PopolazioneNomadelfia\Actions\EntrataMaggiorenneConFamigliaAction;
+use Domain\Nomadelfia\PopolazioneNomadelfia\Actions\EntrataMaggiorenneSingleAction;
+use Domain\Nomadelfia\PopolazioneNomadelfia\Actions\EntrataMinorenneAccoltoAction;
+use Domain\Nomadelfia\PopolazioneNomadelfia\Actions\EntrataMinorenneConFamigliaAction;
+use Domain\Nomadelfia\PopolazioneNomadelfia\Models\PopolazioneNomadelfia;
+use Illuminate\Http\Request;
+
+final class PersonaEntrataController
+{
+    public function create($idPersona)
+    {
+        $persona = Persona::findOrFail($idPersona);
+
+        return view('nomadelfia.persone.inserimento.entrata', compact('persona'));
+    }
+
+    public function store(EntrataPersonaRequest $request, $idPersona)
+    {
+        $request->validated();
+
+        $persona = Persona::findOrFail($idPersona);
+        $data_entrata = Carbon::parse($request->input('data_entrata'));
+
+        switch ($request->tipologia) {
+            case 'dalla_nascita':
+                $famiglia = Famiglia::findOrFail($request->input('famiglia_id'));
+                $action = app(EntrataDallaNascitaAction::class);
+                $action->execute($persona, $famiglia);
+                break;
+            case 'minorenne_accolto':
+                $famiglia = Famiglia::findOrFail($request->input('famiglia_id'));
+                $action = app(EntrataMinorenneAccoltoAction::class);
+                $action->execute($persona, $data_entrata, $famiglia);
+                break;
+            case 'minorenne_famiglia':
+                $famiglia = Famiglia::findOrFail($request->input('famiglia_id'));
+                $action = app(EntrataMinorenneConFamigliaAction::class);
+                $action->execute($persona, $data_entrata, $famiglia);
+                break;
+            case 'maggiorenne_single':
+                $gruppoFamiliare = GruppoFamiliare::findOrFail($request->input('gruppo_id'));
+                $action = app(EntrataMaggiorenneSingleAction::class);
+                $action->execute($persona, $data_entrata, $gruppoFamiliare);
+                break;
+            case 'maggiorenne_famiglia':
+                $gruppoFamiliare = GruppoFamiliare::findOrFail($request->input('gruppo_id'));
+                $act = app(EntrataMaggiorenneConFamigliaAction::class);
+                $act->execute($persona, $data_entrata, $gruppoFamiliare);
+                break;
+            default:
+                return redirect()->back()->withErrore("Tipologia di entrata per $request->tipologia non riconosciuta.");
+
+        }
+
+        return redirect()->route('nomadelfia.persone.dettaglio',
+            [$persona->id])->withSuccess('Persona '.$persona->nominativo.'inserita correttamente.');
+    }
+
+    public function update(Request $request, $idPersona, $entrata)
+    {
+        $request->validate([
+            'data_entrata' => 'date',
+        ], [
+            'data_entrata.date' => 'La data entrata non è valida.',
+        ]);
+        $persona = Persona::findOrFail($idPersona);
+        PopolazioneNomadelfia::query()
+            ->where('persona_id', $persona->id)
+            ->where('data_entrata', $entrata)
+            ->update(['data_entrata' => $request->data_entrata]);
+
+        return redirect()->back()->withSuccess("Data entrata di $persona->nominativo modificata con successo.");
+    }
+}
