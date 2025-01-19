26150574ddcf9a02b834bd5e87294347e4065da6
Davide N <davideneri18@gmail.com>
2025-01-19 23:15:10 +0100

    refactor(app.js): improve formatting and readability of form submission handler

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,23 +14,21 @@ const app = new Vue({
   },
 });
 
-
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
-
 $(function () {
-      // disabled all the input element in a form when a form is submitted
-      $(function () {
-        $("form").submit(function () {
-          $(this)
-            .find(":input")
-            .filter(function () {
-              return !this.value;
-            })
-            .attr("disabled", "disabled");
-          return true; // ensure form still submits
-        });
-      });
- });
+  // disabled all the input element in a form when a form is submitted
+  $(function () {
+    $("form").submit(function () {
+      $(this)
+        .find(":input")
+        .filter(function () {
+          return !this.value;
+        })
+        .attr("disabled", "disabled");
+      return true; // ensure form still submits
+    });
+  });
+});
