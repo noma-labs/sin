351ab5ce910aaa8b37ff8cd08af697a650f9e99c
Davide N <davideneri18@gmail.com>
2025-01-20 00:12:50 +0100

    refactor: remove MyModal component and related references

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,6 @@ import Vue from "vue";
 window.Vue = Vue; //this is important! Do not use require('vue')
 Vue.config.devtools = true;
 
-Vue.component("my-modal", require("./components/my-modal.vue").default);
-
 // create Vue instance
 const app = new Vue({
   el: "#archivio",
