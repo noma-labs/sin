2b3dff219d8caff2de82f68a9dfe3f2973daf7b4
Davide N <davideneri18@gmail.com>
2025-01-21 22:33:06 +0100

    hoome:: update container class for improved layout consistency of cards

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,14 +101,13 @@
                 </div>
             </div>
         </nav>
-        <div class="container-fluid mt-3">
+        <div class="container mt-3">
             @include("partials.flash")
             @include("partials.errors")
             @yield("content")
         </div>
 
         <!-- <script type="text/javascript" src="{{ asset("js/app.js") }}"></script> -->
-
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
