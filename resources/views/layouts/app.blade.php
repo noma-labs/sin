f3858d16c272a058c50497a2b9a912565a6ed64c
Davide N <davideneri18@gmail.com>
2025-01-25 22:44:33 +0100

    refactor(layout): update Bootstrap to version 5.3.3 and enable dark theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}" data-bs-theme="dark">
     <head>
         <meta charset="utf-8" />
         <title>@yield("title")</title>
@@ -9,18 +9,14 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <!-- Bootstrap CSS -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-            crossorigin="anonymous"
-        />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 
         <!-- <link rel="stylesheet" href="{{ asset("css/app.css") }}" />
         <link rel="stylesheet" href="{{ asset("css/sin-theme.css") }}" /> -->
     </head>
     <body>
-        <nav class="navbar navbar-expand-md bg-light">
+        <nav class="navbar navbar-expand-md">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url("/home") }}">
                     <img src="/images/logo-noma.png" alt="" height="40" />
@@ -108,10 +104,7 @@
 
         <!-- <script type="text/javascript" src="{{ asset("js/app.js") }}"></script> -->
         <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"
-        ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     </body>
 </html>
