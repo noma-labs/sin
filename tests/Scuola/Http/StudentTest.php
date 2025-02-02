d9b70b000aff25a0962e9e1389546db00d54af6c
Davide <davideneri18@gmail.com>
2025-02-02 22:09:13 +0100

    chore(scuola): improve student show page (#210)
    * feat(scuola): enhance student detail view with class and work information
    
    * test(scuola): add test for rendering student show page
    
    * fix(scuola): resolve undefined method calls in StudentController

---
+++ tests/Scuola/Http/StudentTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Http\Scuola;
+
+use App\Scuola\Controllers\StudentController;
+use App\Scuola\Models\Anno;
+use App\Scuola\Models\ClasseTipo;
+use App\Scuola\Models\Elaborato;
+use App\Scuola\Models\Studente;
+use Carbon\Carbon;
+
+it('can render the student show page', function (): void {
+    $student = Studente::factory()->minorenne()->maschio()->create();
+    $a = Anno::createAnno(2029);
+    $c = $a->aggiungiClasse(ClasseTipo::all()->random());
+    $c->aggiungiAlunno($student, Carbon::now());
+
+    $elaborato = Elaborato::factory()->create();
+    $elaborato->studenti()->sync($student);
+
+    login();
+    $this->get(action([StudentController::class, 'show'], $student->id))
+        ->assertStatus(200)
+        ->assertSee($student->nome)
+        ->assertSee($elaborato->titolo);
+});
