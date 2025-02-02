e4b67ed43c39a5e3cc24f30faf928eac98c3cd42
Davide N <davideneri18@gmail.com>
2025-02-02 22:02:33 +0100

    fix(scuola): resolve undefined method calls in StudentController

--- src/App/Sin/Scuola/Controllers/StudentController.php
+++ src/App/Sin/Scuola/Controllers/StudentController.php
@@ -12,21 +12,22 @@ final class StudentController
 {
     public function show($id)
     {
-        $student = Studente::select('id', 'nome', 'cognome','data_nascita', 'cf', 'sesso')->findOrFail($id);
+        $student = Studente::select('id', 'nome', 'cognome', 'data_nascita', 'cf', 'sesso')->findOrFail($id);
 
         $classes = Classe::select('classi.id', 'tipo.nome as tipo_nome', 'tipo.ciclo as tipo_ciclo', 'anno.id as anno_id', 'anno.scolastico as anno_scolastico')
-                ->join('tipo', 'classi.tipo_id', '=', 'tipo.id')
-                ->join('anno', 'classi.anno_id', '=', 'anno.id')
-                ->join('alunni_classi', 'classi.id', '=', 'alunni_classi.classe_id')
-                ->where('alunni_classi.persona_id', $student->id)
-                ->orderBy('anno.scolastico', 'ASC')
-                ->get();
+            ->join('tipo', 'classi.tipo_id', '=', 'tipo.id')
+            ->join('anno', 'classi.anno_id', '=', 'anno.id')
+            ->join('alunni_classi', 'classi.id', '=', 'alunni_classi.classe_id')
+            ->where('alunni_classi.persona_id', $student->id)
+            ->orderBy('anno.scolastico', 'ASC')
+            ->get();
 
         $works = Elaborato::select('elaborati.id', 'elaborati.titolo', 'elaborati.anno_scolastico', 'elaborati.classi')
-                ->join('elaborati_studenti', 'elaborati.id', '=', 'elaborati_studenti.elaborato_id')
-                ->where('elaborati_studenti.studente_id', $student->id)
-                ->orderBy('elaborati.anno_scolastico', 'ASC')
-                ->get();
+            ->join('elaborati_studenti', 'elaborati.id', '=', 'elaborati_studenti.elaborato_id')
+            ->where('elaborati_studenti.studente_id', $student->id)
+            ->orderBy('elaborati.anno_scolastico', 'ASC')
+            ->get();
+
         return view('scuola.student.show', compact('student', 'classes', 'works'));
     }
 }
