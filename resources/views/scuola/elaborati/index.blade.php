11796db20ef0d2887e4db5c31bc2a449f8cfe788
Davide <davideneri18@gmail.com>
2025-02-03 23:58:09 +0100

    feat(scuola): enhance elaborati index page with cards (#213)
    * feat(scuola): enhance elaborati index page with dropdown sorting and updated layout
    
    * feat(scuola): update elaborati index layout with card design and improved responsiveness
    
    * style(scuola): clean up HTML formatting in elaborati index view

--- resources/views/scuola/elaborati/index.blade.php
+++ resources/views/scuola/elaborati/index.blade.php
@@ -1,77 +1,89 @@
 @extends("scuola.index")
 
 @section("content")
-    @include("partials.header", ["title" => "Elaborati"])
-
-    <a
-        class="btn btn-primary my-2"
-        href="{{ route("scuola.elaborati.create") }}"
-        role="button"
-    >
-        Inserisci
-    </a>
-
-    <a
-        class="btn btn-secondary my-2"
-        href="{{ route("scuola.elaborati.index", ["order" => "collocazione", "by" => "ASC"]) }}"
-        role="button"
-    >
-        Ordina per Collocazione
-    </a>
-    <a
-        class="btn btn-secondary my-2"
-        href="{{ route("scuola.elaborati.index", ["order" => "anno_scolastico", "by" => "ASC"]) }}"
-        role="button"
-    >
-        Ordina per Anno
-    </a>
-
-    <div class="card">
-        <div class="card-header">
-            Lista Elaborati
-            <span class="fw-bold">({{ $elaborati->count() }})</span>
-        </div>
-        <ul class="list-group list-group-flush">
-            @forelse ($elaborati as $elaborato)
-                <li class="list-group-item">
-                    <span class="badge bg-warning">
-                        {{ $elaborato->anno_scolastico }}
-                    </span>
-
-                    <span class="badge bg-primary">
-                        {{ $elaborato->collocazione }}
-                    </span>
-
-                    <strong>{{ $elaborato->titolo }}</strong>
-
-                    @if ($elaborato->file_path)
-                        <span class="badge bg-danger">pdf</span>
-                    @endif
-
-                    <span class="badge bg-secondary">
-                        {{ strtolower($elaborato->rilegatura) }}
-                    </span>
-
-                    {{ strtolower($elaborato->note) }}
-
-                    <!-- TODO: autore is taken from the "old" libro table and should be removed. It is only needed to have the old info for copying it into the new one -->
-                    @if ($elaborato->autore)
-                        <span class="alert alert-warning small">
-                            {{ $elaborato->autore }}
-                        </span>
-                    @endif
-
-                    <a
-                        href="{{ route("scuola.elaborati.show", $elaborato->id) }}"
-                        class="btn btn-primary"
-                        role="button"
-                    >
-                        Dettaglio
-                    </a>
-                </li>
-            @empty
-                <li class="list-group-item">Nessun elaborato disponibile.</li>
-            @endforelse
+    @include("partials.header", ["title" => "Elaborati ", "subtitle" => $elaborati->count()])
+    <div class="mb-3 d-flex justify-content-between">
+        <a
+            class="btn btn-secondary dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        >
+            Ordina per
+        </a>
+        <ul class="dropdown-menu">
+            <li>
+                <a
+                    class="dropdown-item"
+                    href="{{ route("scuola.elaborati.index", ["order" => "collocazione", "by" => "ASC"]) }}"
+                >
+                    Collocazione
+                </a>
+            </li>
+            <li>
+                <a
+                    class="dropdown-item"
+                    href="{{ route("scuola.elaborati.index", ["order" => "anno_scolastico", "by" => "ASC"]) }}"
+                >
+                    Anno
+                </a>
+            </li>
         </ul>
+
+        <a
+            class="btn btn-primary"
+            href="{{ route("scuola.elaborati.create") }}"
+            role="button"
+        >
+            Inserisci
+        </a>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-4 row-cols-lg-6 g-3">
+        @foreach ($elaborati as $elaborato)
+            <div class="col">
+                <div class="card h-100">
+                    <!-- <img
+                        src="/images/logo-noma.png"
+                        class="card-img-top img-fluid p-3"
+                    /> -->
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $elaborato->titolo }}</h5>
+
+                        {{ strtolower($elaborato->note) }}
+
+                        <!-- TODO: autore is taken from the "old" libro table and should be removed. It is only needed to have the old info for copying it into the new one -->
+                        @if ($elaborato->autore)
+                            <div class="alert alert-warning" role="alert">
+                                {{ $elaborato->autore }}
+                            </div>
+                        @endif
+
+                        <a
+                            href="{{ route("scuola.elaborati.show", $elaborato->id) }}"
+                            class="stretched-link"
+                        ></a>
+                    </div>
+                    <div class="card-footer">
+                        <span class="badge rounded-pill bg-secondary">
+                            {{ $elaborato->anno_scolastico }}
+                        </span>
+
+                        @if ($elaborato->collocazione)
+                            <span class="badge rounded-pill bg-primary">
+                                {{ $elaborato->collocazione }}
+                            </span>
+                        @endif
+
+                        @if ($elaborato->file_path)
+                            <span class="badge rounded-pill bg-danger">
+                                pdf
+                            </span>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
