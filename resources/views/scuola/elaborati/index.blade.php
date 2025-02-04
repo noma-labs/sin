365d508ac7aaf0a9c250a4b7079c87ad34a98a9c
Davide <davideneri18@gmail.com>
2025-02-04 23:21:47 +0100

    feat(scuola): add elaborati view choiche (table, cards) (#214)
    * feat(scuola): add view toggle and sorting options to elaborati index page
    
    * fix(scuola): remove unnecessary class from elaborati index table rows

--- resources/views/scuola/elaborati/index.blade.php
+++ resources/views/scuola/elaborati/index.blade.php
@@ -3,33 +3,62 @@
 @section("content")
     @include("partials.header", ["title" => "Elaborati ", "subtitle" => $elaborati->count()])
     <div class="mb-3 d-flex justify-content-between">
-        <a
-            class="btn btn-secondary dropdown-toggle"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-        >
-            Ordina per
-        </a>
-        <ul class="dropdown-menu">
-            <li>
-                <a
-                    class="dropdown-item"
-                    href="{{ route("scuola.elaborati.index", ["order" => "collocazione", "by" => "ASC"]) }}"
-                >
-                    Collocazione
-                </a>
-            </li>
-            <li>
-                <a
-                    class="dropdown-item"
-                    href="{{ route("scuola.elaborati.index", ["order" => "anno_scolastico", "by" => "ASC"]) }}"
-                >
-                    Anno
-                </a>
-            </li>
-        </ul>
+        <div>
+            <a
+                class="btn btn-secondary dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
+                Ordina per
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a
+                        class="dropdown-item"
+                        href="{{ route("scuola.elaborati.index", ["order" => "collocazione", "by" => "ASC", "view" => $view]) }}"
+                    >
+                        Collocazione
+                    </a>
+                </li>
+                <li>
+                    <a
+                        class="dropdown-item"
+                        href="{{ route("scuola.elaborati.index", ["order" => "anno_scolastico", "by" => "ASC", "view" => $view]) }}"
+                    >
+                        Anno
+                    </a>
+                </li>
+            </ul>
+            <a
+                class="btn btn-secondary dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+            >
+                Vista
+            </a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a
+                        class="dropdown-item"
+                        href="{{ route("scuola.elaborati.index", ["view" => "table"]) }}"
+                    >
+                        Tabella
+                    </a>
+                </li>
+                <li>
+                    <a
+                        class="dropdown-item"
+                        href="{{ route("scuola.elaborati.index", ["view" => "cards"]) }}"
+                    >
+                        Griglia
+                    </a>
+                </li>
+            </ul>
+        </div>
 
         <a
             class="btn btn-primary"
@@ -39,51 +68,106 @@
             Inserisci
         </a>
     </div>
-
-    <div class="row row-cols-1 row-cols-md-4 row-cols-lg-6 g-3">
-        @foreach ($elaborati as $elaborato)
-            <div class="col">
-                <div class="card h-100">
-                    <!-- <img
+    @if ($view === "table")
+        <div class="table-responsive">
+            <table class="table table-hover table-sm">
+                <thead>
+                    <tr class="table-warning">
+                        <th scope="col">#</th>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Anno Scolastico</th>
+                        <th scope="col">Collocazione</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Dimensione</th>
+                        <th scope="col">Note</th>
+                        <th scope="col">Azioni</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($elaborati as $elaborato)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration }}</th>
+                            <td>{{ $elaborato->titolo }}</td>
+                            <td>{{ $elaborato->anno_scolastico }}</td>
+                            <td>{{ $elaborato->collocazione }}</td>
+                            <td>{{ $elaborato->file_mime_type }}</td>
+                            <td>
+                                {{ Illuminate\Support\Number::fileSize($elaborato->file_size) }}
+                            </td>
+                            <td>{{ strtolower($elaborato->note) }}</td>
+                            <td>
+                                <a
+                                    href="{{ route("scuola.elaborati.show", $elaborato->id) }}"
+                                    class="btn btn-primary btn-sm"
+                                >
+                                    Visualizza
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    @else
+        <div class="row row-cols-1 row-cols-md-4 row-cols-lg-6 g-3">
+            @foreach ($elaborati as $elaborato)
+                <div class="col">
+                    <div class="card h-100">
+                        <!-- <img
                         src="/images/logo-noma.png"
                         class="card-img-top img-fluid p-3"
                     /> -->
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $elaborato->titolo }}</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ $elaborato->titolo }}
+                            </h5>
 
-                        {{ strtolower($elaborato->note) }}
+                            {{ strtolower($elaborato->note) }}
 
-                        <!-- TODO: autore is taken from the "old" libro table and should be removed. It is only needed to have the old info for copying it into the new one -->
-                        @if ($elaborato->autore)
-                            <div class="alert alert-warning" role="alert">
-                                {{ $elaborato->autore }}
-                            </div>
-                        @endif
+                            <!-- TODO: autore is taken from the "old" libro table and should be removed. It is only needed to have the old info for copying it into the new one -->
+                            @if ($elaborato->autore)
+                                <div class="alert alert-warning" role="alert">
+                                    {{ $elaborato->autore }}
+                                </div>
+                            @endif
 
-                        <a
-                            href="{{ route("scuola.elaborati.show", $elaborato->id) }}"
-                            class="stretched-link"
-                        ></a>
-                    </div>
-                    <div class="card-footer">
-                        <span class="badge rounded-pill bg-secondary">
-                            {{ $elaborato->anno_scolastico }}
-                        </span>
-
-                        @if ($elaborato->collocazione)
-                            <span class="badge rounded-pill bg-primary">
-                                {{ $elaborato->collocazione }}
+                            <a
+                                href="{{ route("scuola.elaborati.show", $elaborato->id) }}"
+                                class="stretched-link"
+                            ></a>
+                        </div>
+                        <div class="card-footer">
+                            <span class="badge rounded-pill bg-secondary">
+                                {{ $elaborato->anno_scolastico }}
                             </span>
-                        @endif
 
-                        @if ($elaborato->file_path)
-                            <span class="badge rounded-pill bg-danger">
-                                pdf
-                            </span>
-                        @endif
+                            @if ($elaborato->collocazione)
+                                <span class="badge rounded-pill bg-primary">
+                                    {{ $elaborato->collocazione }}
+                                </span>
+                            @endif
+
+                            @if ($elaborato->file_path)
+                                <span
+                                    class="badge rounded-pill bg-dark-subtle text-dark-emphasis"
+                                >
+                                    {{ $elaborato->file_mime_type }}
+                                </span>
+                                <span
+                                    class="badge rounded-pill bg-dark-subtle text-dark-emphasis"
+                                >
+                                    {{ Illuminate\Support\Number::fileSize($elaborato->file_size) }}
+                                </span>
+                            @endif
+
+                            <a
+                                href="{{ route("scuola.elaborati.preview", $elaborato->id) }}"
+                                class="stretched-link"
+                            ></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
-    </div>
+            @endforeach
+        </div>
+    @endif
 @endsection
