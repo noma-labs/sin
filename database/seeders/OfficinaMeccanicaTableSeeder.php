3147ec743a102449d6affe56cedd52b5af5cfe7e
MatteNeri <matteo2794@gmail.com>
2025-06-24 11:27:24 +0200

    feat(meccanica): changed seeder to generate all kind of vehicles

--- database/seeders/OfficinaMeccanicaTableSeeder.php
+++ database/seeders/OfficinaMeccanicaTableSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Nomadelfia\Azienda\Models\Azienda;
 use App\Nomadelfia\Persona\Models\Persona;
+use App\Officina\Models\Impiego;
 use App\Officina\Models\Prenotazioni;
 use App\Officina\Models\Veicolo;
 use Carbon\Carbon;
@@ -23,24 +24,57 @@ final class OfficinaMeccanicaTableSeeder extends Seeder
             'data_inizio_azienda' => Carbon::now()->toDateString(),
             'mansione' => Azienda::MANSIONE_RESPONSABILE,
         ]);
-        $veicoli = Veicolo::factory(5)
-            ->impiegoGrosseto()
-            ->tipologiaMacchina()
-            ->create();
 
-        $personale = Veicolo::factory(2)
-            ->impiegoPersonale()
-            ->tipologiaMacchina()
+        // Array dei metodi di tipologia
+        $tipologie = [
+            'tipologiaAutovettura',
+            'tipologiaAutocarro',
+            'tipologiaAutobus',
+            'tipologiaCamper',
+            'tipologiaCiclomotore',
+            'tipologiaFurgoncino',
+            'tipologiaFurgone',
+            'tipologiaMezzoAgricolo',
+            'tipologiaMotocarro',
+            'tipologiaMotociclo',
+            'tipologiaRimorchio',
+            'tipologiaTrattore',
+            'tipologiaVeicoloEdile',
+        ];
+
+        // Array dei metodi di impiego
+        $impieghi = [
+            'impiegoGrosseto',
+            'impiegoPersonale',
+            'impiegoInterno',
+            'impiegoViaggiLunghi',
+            'impiegoRoma',
+            'impiegoAutobus',
+        ];
+
+        // Crea un veicolo per ogni combinazione tipologia/impiego
+        foreach ($tipologie as $tipologia) {
+            foreach ($impieghi as $impiego) {
+                $randomCount = rand(1, 5); // Numero casuale di veicoli da creare per ogni combinazione
+                Veicolo::factory($randomCount)
+                    ->{$impiego}()
+                    ->{$tipologia}()
+                    ->create();
+            }
+        }
+
+        $impiegoGrossetoId = Impiego::where('nome', 'Grosseto')->first()->id;
+        $veicoloGrosseto = Veicolo::where('impiego_id', $impiegoGrossetoId)->first();
+        $veicoloGrosseto2 = Veicolo::where('impiego_id', $impiegoGrossetoId)->skip(1)->first();
+
+        Prenotazioni::factory()
+            ->prenotata(Carbon::now(), Carbon::now()->addHour(2))
+            ->veicolo($veicoloGrosseto)
             ->create();
 
         Prenotazioni::factory()
             ->prenotata(Carbon::now(), Carbon::now()->addHour(2))
-            ->veicolo($veicoli->random())
-            ->create();
-
-        Prenotazioni::factory()
-            ->prenotata(Carbon::now(), Carbon::now()->addHour(2))
-            ->veicolo($personale->random())
+            ->veicolo($veicoloGrosseto2)
             ->create();
     }
 }
