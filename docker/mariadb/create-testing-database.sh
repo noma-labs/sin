4332ab7ec4ae6fab79cb11431bfac9c6d03e7b9a
dido18 <davideneri18@gmail.com>
2025-12-19 18:42:39 +0100

    Add Docker setup for Laravel Sail with MariaDB and phpMyAdmin

---
+++ docker/mariadb/create-testing-database.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# TODO: Enable this script when needed
+# /usr/bin/mariadb --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
+#     CREATE DATABASE IF NOT EXISTS testing;
+#     GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
+# EOSQL
