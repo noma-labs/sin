5b90c844ec7ac4cca804cf7578d3cd02cb78b585
dido18 <davideneri18@gmail.com>
2026-01-21 22:08:45 +0100

    feat(migration): enforce strict types and mark class as final in CreateViewDbfEnrico

--- database/migrations/foto/2026_01_19_214533_create_view_dbf_enrico.php
+++ database/migrations/foto/2026_01_19_214533_create_view_dbf_enrico.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use SqlMigrations\SqlMigration;
 
-class CreateViewDbfEnrico extends SqlMigration
+final class CreateViewDbfEnrico extends SqlMigration
 {
     public $connection = 'db_foto';
 }
