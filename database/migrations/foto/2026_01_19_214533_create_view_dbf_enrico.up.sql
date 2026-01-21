00f03c57f40d5d140f6c01da005c4d2b29b282c8
dido18 <davideneri18@gmail.com>
2026-01-21 22:04:16 +0100

    add view

--- database/migrations/foto/2026_01_19_214533_create_view_dbf_enrico.up.sql
+++ database/migrations/foto/2026_01_19_214533_create_view_dbf_enrico.up.sql
@@ -1,42 +1,18 @@
-
-
--- come data string has bad format like '03/03/-1'
-ALTER TABLE dbf_foto_enrico
-ADD COLUMN data_ok DATE;
+-- change nfo to int
+ALTER TABLE dbf_dia120_enrico
+MODIFY COLUMN nfo INT;
+-- treat empty strings as 0
 UPDATE dbf_foto_enrico
-SET data_ok = STR_TO_DATE(
-    CONCAT(
-        SUBSTRING_INDEX(data, '/', 2),  -- month/day
-        '/',
-        CASE
-            WHEN CAST(SUBSTRING_INDEX(data, '/', -1) AS UNSIGNED) BETWEEN 0 AND 25
-                THEN CONCAT('20', LPAD(SUBSTRING_INDEX(data, '/', -1), 2, '0'))
-            ELSE
-                CONCAT('19', LPAD(SUBSTRING_INDEX(data, '/', -1), 2, '0'))
-        END
-    ),
-    '%m/%d/%Y'
-)
-WHERE data REGEXP '^[0-9]{2}/[0-9]{2}/[0-9]{2}$';
+SET nfo = 0
+WHERE nfo = '';
 ALTER TABLE dbf_foto_enrico
-CHANGE COLUMN data data_orig VARCHAR(20),
-CHANGE COLUMN data_ok data DATE;
+MODIFY COLUMN nfo INT;
 
-ALTER TABLE dbf_dia120_enrico
-ADD COLUMN IF NOT EXISTS data_orig VARCHAR(20);
-UPDATE dbf_dia120_enrico
-SET data_orig = data;
-ALTER TABLE dbf_dia120_enrico
-ADD COLUMN IF NOT EXISTS data_ok DATE;
-UPDATE dbf_dia120_enrico
-SET data_ok = STR_TO_DATE(data, '%Y-%m-%d')
-WHERE data REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$';
-ALTER TABLE dbf_dia120_enrico
-CHANGE COLUMN data data_orig VARCHAR(20),
-CHANGE COLUMN data_ok data DATE;
+ALTER TABLE dbf_slide_enrico
+MODIFY COLUMN nfo INT;
 
+-- create a unified view
 CREATE VIEW dbf_all AS
-
 SELECT
   'dia120' AS source,
   datnum,
