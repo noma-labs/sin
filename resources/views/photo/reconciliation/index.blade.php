5eb58968036b47cbd7b9d9204241b1c9d8f4e9c2
Davide <davideneri18@gmail.com>
2026-04-09 18:02:40 +0200

    feat(reconciliation): add pagination for DBF records in reconciliation view (#383)

--- resources/views/photo/reconciliation/index.blade.php
+++ resources/views/photo/reconciliation/index.blade.php
@@ -301,6 +301,12 @@
                             @endforelse
                         </div>
                     </div>
+
+                    @if ($dbfAllRecords->hasPages())
+                        <div class="card-footer bg-light">
+                            {{ $dbfAllRecords->links() }}
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
