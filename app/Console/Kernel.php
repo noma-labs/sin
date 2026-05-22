babb91c3db09d4a5d9eb0766ab5ebbc31b41c6c3
dido18 <davideneri18@gmail.com>
2026-05-22 22:28:41 +0200

    feat: remove RecordingSynchCommand references from Kernel and phpstan baseline

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,7 +8,6 @@ use App\Console\Commands\CreateDatabaseCommand;
 use App\Console\Commands\DetectPhotoIssuesCommand;
 use App\Console\Commands\ExifExtractCommand;
 use App\Console\Commands\ExifJsonImportCommand;
-use App\Console\Commands\RecordingSynchCommand;
 use App\Console\Commands\SynchPeopleOnPhotosCommand;
 use App\Console\Commands\TranscriptsImportCommand;
 use App\Console\Commands\TranscriptSynchCommand;
@@ -29,7 +28,6 @@ final class Kernel extends ConsoleKernel
         SynchPeopleOnPhotosCommand::class,
         DetectPhotoIssuesCommand::class,
         TranscriptsImportCommand::class,
-        RecordingSynchCommand::class,
         TranscriptSynchCommand::class,
     ];
 
