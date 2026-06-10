155675e8b65e540138c620198bf139bad7d9131d
dido18 <davideneri18@gmail.com>
2026-06-10 18:20:51 +0200

    litn

--- resources/views/archive/index.blade.php
+++ resources/views/archive/index.blade.php
@@ -263,12 +263,12 @@
           </div>
           <div class="card-body p-2">
             @if ($selectedDoc->audio)
-              <audio
-                controls
-                class="w-100 mb-3"
-                style="height: 36px"
-                src="{{ route('archive.audio', $selectedDoc->audio->id) }}"
-              ></audio>
+              <audio controls class="w-100 mb-3" style="height: 36px">
+                <source
+                  src="{{ route('archive.audio', $selectedDoc->audio->id) }}"
+                  type="audio/mpeg"
+                />
+              </audio>
             @endif
             @if ($selectedDoc->transcript)
               <div class="bg-light rounded-2 p-3 border">
